2513cd0b2c721901f64882c126105e3047ec3aaf
praharthanan <praharthanan@users.noreply.github.com>
2016-03-23 17:55:45 +0530

    Update database.php

--- app/config/database.php
+++ app/config/database.php
@@ -48,7 +48,7 @@ return array(
         'mysql' => array(
             'driver' => 'mysql',
             'host' => 'localhost',
-            'database' => 'address_books',
+            'database' => 'address_book',
             'username' => 'root',
             'password' => '',
             'charset' => 'utf8',
